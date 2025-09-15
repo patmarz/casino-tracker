660e8c9d7eed010739ba6b882ee10df373022371
patmarz <patrykmarz24@gmail.com>
2025-09-14 20:07:56 -0500

    Commit 1.05

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION="1.0.4";const CACHE="casino-cache-"+VERSION;const ASSETS=["./","./index.html?rev="+VERSION,"./manifest.json?rev="+VERSION,"./icons/icon-192.png","./icons/icon-512.png"];
+const VERSION="1.1.0";const CACHE="casino-cache-"+VERSION;const ASSETS=["./","./index.html?rev="+VERSION,"./manifest.json?rev="+VERSION,"./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("casino-cache-")&&k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener("fetch",e=>{const r=e.request;if(r.method!=="GET")return;e.respondWith(caches.match(r).then(c=>c||fetch(r).then(res=>{const cp=res.clone();caches.open(CACHE).then(ca=>ca.put(r,cp));return res}).catch(()=>caches.match("./index.html?rev="+VERSION))))});
